3d25e222d1ab98023584daefa82449810033e333
Andrew Singer <andrewsinger@andrews-mbp-3.attlocal.net>
2019-10-27 22:43:39 -0700

    Create Teacher schema

--- models/teachers.js
+++ models/teachers.js
@@ -4,6 +4,15 @@ const uniqueValidator = ('mongoose-unique-validator');
 const teacherSchema = mongoose.Schema({
     name: String,
     email: { type: String, lowercase: true, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    students: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student'
+    }]
+});
 
-})
+teacherSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+const Teacher = mongoose.model('Teacher', teacherSchema);
+
+module.exports = Teacher
