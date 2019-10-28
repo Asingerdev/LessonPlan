bef9156baa23added56b3be98e38c3a9abe0c055
Andrew Singer <andrewsinger@andrews-mbp-3.attlocal.net>
2019-10-27 22:48:41 -0700

    Create student schema

--- models/students.js
+++ models/students.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const uniqueValidator = ('mongoose-unique-validator');
+
+const studentSchema = mongoose.Schema({
+    name: String,
+    email: { type: String, lowercase: true, required: true, unique: true },
+    password: { type: String, required: true },
+    songs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Song'
+    }]
+});
+
+studentSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
