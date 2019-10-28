b5c2146e01a7a6c7ebf94eba39be62741c0285be
Andrew Singer <andrewsinger@andrews-mbp-3.attlocal.net>
2019-10-27 23:00:02 -0700

    Install middleware method override

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const PORT = 3000;
 require('./db/db');
+
+// Middleware
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.listen(PORT, () => {
     console.log('App is running on port: ', PORT);
